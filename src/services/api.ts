522b995fb71446f7f3100a0d36d07e03e321d194
Muditha <mudithcse@gmail.com>
2025-10-20 20:54:44 +0530

    default update

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://btrust.scm.azurewebsites.net/api/deployments/latest/api',
 });
 
 // Add token to requests if available
